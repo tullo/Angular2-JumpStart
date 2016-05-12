55f5ea40d215042fbf640da1acd219fe5e4b2c97
Andreas Amstutz <andreasamstutz@redpill-linpro.com>
2016-05-12 22:03:33 +0200

    Activate production mode

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,9 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
+import { enableProdMode } from '@angular/core';
+
+enableProdMode();
 
 bootstrap(AppComponent).then(
     success => console.log('AppComponent bootstrapped!'),
